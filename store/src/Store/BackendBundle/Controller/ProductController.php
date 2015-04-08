8db9b6998a0efad398d18bf93c52ca4c2dec171f
Jeremy95 <jerem.b95@gmail.com>
2015-04-08 16:07:03 +0200

    commit form

--- store/src/Store/BackendBundle/Controller/ProductController.php
+++ store/src/Store/BackendBundle/Controller/ProductController.php
@@ -15,7 +15,14 @@ class ProductController extends Controller
      */
     public function listAction()
     {
-        return $this->render('StoreBackendBundle:Product:list.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $products = $em->getRepository('StoreBackendBundle:Product')->findAll();
+
+
+        return $this->render('StoreBackendBundle:Product:list.html.twig', array(
+            'products' => $products
+        ));
     }
 
     /**
